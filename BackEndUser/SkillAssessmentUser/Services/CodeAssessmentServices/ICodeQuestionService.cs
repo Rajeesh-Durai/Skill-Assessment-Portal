27e662ddd29d195c31b28cc3e6d5dfd0a243d515
Rajeesh Durai Thanga Raja <rajeeshdurai.thangaraja@kanini.com>
2024-05-08 23:02:07 +0530

    final commit

---
+++ BackEndUser/SkillAssessmentUser/Services/CodeAssessmentServices/ICodeQuestionService.cs
@@ -0,0 +1,10 @@
+﻿using SkillAssessmentUser.Model.Core_Model;
+using SkillAssessmentUser.Model.View_Model;
+
+namespace SkillAssessmentUser.Services.CodeAssessmentServices
+{
+    public interface ICodeQuestionService
+    {
+        Task<List<RandomQuestionDTO>> GetRandomizedQuestions(string assessmentId, int numberOfQuestions);
+    }
+}
